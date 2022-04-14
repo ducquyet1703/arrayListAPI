0adac0b70736aab81655151a1667d71766064fd6
ducquyet <86555752+ducquyet1703@users.noreply.github.com>
2022-04-14 15:54:00 +0700

    Update arrayFindStrRepository.java

--- src/main/java/com/example/repository/arrayFindStrRepository.java
+++ src/main/java/com/example/repository/arrayFindStrRepository.java
@@ -1,8 +1,9 @@
 package com.example.repository;
 
 import com.example.entity.FindStringEntity;
-import org.omg.CORBA.INTERNAL;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface arrayFindStrRepository extends JpaRepository<FindStringEntity, Integer> {
 }
